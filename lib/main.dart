a5535ee1d62769edfbe23f1e0f9d66580436dee5
NADDZ2510 <anhducstudy2510@gmail.com>
2025-01-20 22:19:47 +0700

    Khoi tao firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/rendering.dart';
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
